fd00a363b326657a3b737c3eac1d8287de92db90
webdevharsh <145906054+webdevharsh@users.noreply.github.com>
2023-09-24 20:03:00 +0530

    Create Menu.js

---
+++ Menu.js
@@ -0,0 +1,7 @@
+//javascritpro_
+let toggle = document.querySelector('.menu .toggle');
+let menu = document.querySelector('.menu');
+
+toggle.addEventListener('click', () => {
+   menu.classList.toggle('active');
+})
